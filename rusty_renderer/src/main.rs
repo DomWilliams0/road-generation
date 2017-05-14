e63f53cbaab10ddc57c5548e9117a108c0f77895
Dom Williams <me@domwillia.ms>
2017-05-14 18:58:37 +0200

    Add render skeleton to renderer

--- rusty_renderer/src/main.rs
+++ rusty_renderer/src/main.rs
@@ -1,6 +1,34 @@
 extern crate rusty_roads;
 
+use rusty_roads::{RoadError, RoadMap};
+use std::process;
+
 fn main() {
 
-    let _ = rusty_roads::RoadmapBuilder::new().size(600, 960).generate();
+    match run() {
+        Err(err) => {
+            println!("Error: {:?}", err);
+            process::exit(1);
+        }
+
+        Ok(_) => process::exit(0),
+    }
+
+}
+
+fn run() -> Result<(), RoadError> {
+
+    let roadmap = rusty_roads::RoadmapBuilder::new()
+        .size(600, 960)
+        .generate()?;
+
+    render(&roadmap)
+}
+
+fn render(roadmap: &RoadMap) -> Result<(), RoadError> {
+
+    let roads = roadmap.roads();
+    println!("TODO: render {} roads", roads.len());
+
+    Ok(())
 }
