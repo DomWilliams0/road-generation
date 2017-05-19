8ffadcae0c534981184a99a4030f748f727b1138
Dom Williams <me@domwillia.ms>
2017-05-19 13:46:49 +0200

    Add idea of road proposals

--- rusty_roads/src/rules/grid.rs
+++ rusty_roads/src/rules/grid.rs
@@ -1,25 +1,28 @@
-use {Road, RoadType, Point};
+use RoadType;
 use rand::{random, Closed01};
 use std::f64::consts::PI;
-use cgmath::{Vector2, Point2, Rad};
-use cgmath::prelude::*;
+use cgmath::Point2;
+use super::{Proposal, Proposals};
 
 pub fn propose_branching_roads(point: &Point2<f64>,
-                               direction: &Vector2<f64>,
+                               cur_angle: f64,
                                road_type: RoadType,
                                branch: bool,
                                road_chance: f64,
                                road_length: f64,
-                               out: &mut Vec<Road>) {
+                               out: &mut Proposals) {
     const GRID_ANGLES: [f64; 3] = [-PI / 2., 0., PI / 2.];
 
-    let cur_angle: Rad<f64> = Rad::atan2(direction.y, direction.x);
-
     if !branch {
-        out.push(propose_single_road(GRID_ANGLES[1], cur_angle, road_length, point, road_type));
+        out[0] = Some(Proposal {
+                          road_type: road_type,
+                          angle: cur_angle + GRID_ANGLES[1], // straight
+                          from: *point,
+                          length: road_length,
+                      });
     } else {
 
-        for grid_angle in &GRID_ANGLES {
+        for (i, grid_angle) in GRID_ANGLES.iter().enumerate() {
 
             // unlucky
             let Closed01(chance) = random::<Closed01<f64>>();
@@ -27,26 +30,12 @@ pub fn propose_branching_roads(point: &Point2<f64>,
                 continue;
             }
 
-            out.push(propose_single_road(*grid_angle, cur_angle, road_length, point, road_type));
+            out[i] = Some(Proposal {
+                              road_type: road_type,
+                              angle: cur_angle + grid_angle,
+                              from: *point,
+                              length: road_length,
+                          });
         }
     }
-
-}
-
-// TODO this isn't specific to grid!
-pub fn propose_single_road(adjusted_angle: f64,
-                           cur_angle: Rad<f64>,
-                           length: f64,
-                           point: &Point2<f64>,
-                           road_type: RoadType)
-                           -> Road {
-
-    let new_angle = cur_angle + Rad(adjusted_angle);
-
-    let new_x = point.x + (Angle::cos(new_angle) * length);
-    let new_y = point.y + (Angle::sin(new_angle) * length);
-
-    Road::new_with_points(road_type,
-                          Point::new(point.x, point.y),
-                          Point::new(new_x, new_y))
 }
--- rusty_roads/src/rules/mod.rs
+++ rusty_roads/src/rules/mod.rs
@@ -1,5 +1,5 @@
 use {Road, RoadType, Point};
-use cgmath::{Vector2, Point2};
+use cgmath::{Vector2, Point2, Angle, Rad};
 use cgmath::prelude::*;
 use config;
 
@@ -12,14 +12,38 @@ enum GenerationRule {
     Organic,
 }
 
-type Proposal = fn(&Point2<f64>,
-                   &Vector2<f64>,
-                   RoadType,
-                   bool,
-                   f64,
-                   f64,
-                   &mut Vec<Road>);
+const MAX_PROPOSALS: usize = 3;
+type Proposals = [Option<Proposal>; MAX_PROPOSALS];
+macro_rules! new_proposals {
+  () => {
+    [None; MAX_PROPOSALS];
+  }
+}
 
+type ProposalGenerator = fn(&Point2<f64>, f64, RoadType, bool, f64, f64, &mut Proposals);
+
+#[derive(Copy, Clone)]
+pub struct Proposal {
+    pub road_type: RoadType,
+    pub angle: f64,
+    pub from: Point2<f64>,
+    pub length: f64,
+}
+impl Proposal {
+    fn to_road(self) -> Road {
+        let angle = Rad(self.angle);
+        let new_x = self.from.x + (Angle::cos(angle) * self.length);
+        let new_y = self.from.y + (Angle::sin(angle) * self.length);
+
+        let arse: Rad<f64> = Rad(20.0f64) + Rad(10.);
+        Angle::cos(arse);
+
+        Road::new_with_points(self.road_type,
+                              Point::new(self.from.x, self.from.y),
+                              Point::new(new_x, new_y))
+
+    }
+}
 pub fn propose_roads(config: &config::GenerationConfig,
                      road: &Road,
                      branch: bool,
@@ -27,17 +51,29 @@ pub fn propose_roads(config: &config::GenerationConfig,
 
     let from = Point2::from(road.from().unwrap().pos);
     let to = Point2::from(road.to().unwrap().pos);
+
     let vec = (to - from).normalize();
+    let Rad(cur_angle) = Rad::atan2(vec.y, vec.x);
+
+
+    let mut proposals: Proposals = new_proposals!();
 
     let rule = get_rule(road.to().as_ref().unwrap());
     if let Some(generator) = get_generator(&rule) {
         (generator)(&to,
-                    &vec,
+                    cur_angle,
                     road.road_type(),
                     branch,
                     config.road_chance,
                     config.road_length,
-                    out);
+                    &mut proposals);
+    }
+
+    for p in proposals
+            .iter()
+            .take_while(|p| p.is_some())
+            .map(|p| p.unwrap()) {
+        out.push(p.to_road());
     }
 }
 
@@ -50,10 +86,10 @@ fn get_rule(point: &Point) -> GenerationRule {
     }
 }
 
-fn get_generator(rule: &GenerationRule) -> Option<Proposal> {
+fn get_generator(rule: &GenerationRule) -> Option<ProposalGenerator> {
     match *rule {
         GenerationRule::Grid => Some(grid::propose_branching_roads),
-        GenerationRule::Organic => Some(organic::propose_branching_roads),
-        // _ => None,
+        // GenerationRule::Organic => Some(organic::propose_branching_roads), // temporary
+        _ => None,
     }
 }
